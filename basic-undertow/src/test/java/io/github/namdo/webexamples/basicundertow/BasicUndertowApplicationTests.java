9333c3ddb3086a0887e6c8217ea718cb8da4e663
Nam Do <ndo@overstock.com>
2020-11-12 23:00:18 -0700

    Basic WebFlux app with Undertow

---
+++ basic-undertow/src/test/java/io/github/namdo/webexamples/basicundertow/BasicUndertowApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.namdo.webexamples.basicundertow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicUndertowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
