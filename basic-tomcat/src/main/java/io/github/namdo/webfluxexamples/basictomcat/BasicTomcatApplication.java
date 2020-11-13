cb67915f958dd06b81648e82df0b5b93e7e39c70
Nam Do <ndo@overstock.com>
2020-11-12 22:54:35 -0700

    Basic WebFlux app with Tomcat

---
+++ basic-tomcat/src/main/java/io/github/namdo/webfluxexamples/basictomcat/BasicTomcatApplication.java
@@ -0,0 +1,13 @@
+package io.github.namdo.webfluxexamples.basictomcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicTomcatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicTomcatApplication.class, args);
+	}
+
+}
---
+++ basic-tomcat/src/test/java/io/github/namdo/webfluxexamples/basictomcat/BasicTomcatApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.namdo.webfluxexamples.basictomcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicTomcatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
