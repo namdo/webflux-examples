35e739acc8aaf488d00c775422f3340409d1bb45
Nam Do <ndo@overstock.com>
2020-11-12 22:48:29 -0700

    Basic WebFlux app with Jetty

---
+++ basic-jetty/src/main/java/io/github/namdo/webfluxexamples/basicjetty/BasicJettyApplication.java
@@ -0,0 +1,13 @@
+package io.github.namdo.webfluxexamples.basicjetty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicJettyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicJettyApplication.class, args);
+	}
+
+}
---
+++ basic-jetty/src/test/java/io/github/namdo/webfluxexamples/basicjetty/BasicJettyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.namdo.webfluxexamples.basicjetty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicJettyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
