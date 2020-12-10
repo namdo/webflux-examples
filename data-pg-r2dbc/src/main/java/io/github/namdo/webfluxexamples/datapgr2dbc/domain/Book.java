81bffc02c2ab9f6bd406e15b74d3189f78a899e5
Nam Do <ndo@overstock.com>
2020-12-09 23:47:28 -0700

    Initial commit

---
+++ data-pg-r2dbc/src/main/java/io/github/namdo/webfluxexamples/datapgr2dbc/domain/Book.java
@@ -0,0 +1,24 @@
+package io.github.namdo.webfluxexamples.datapgr2dbc.domain;
+
+import lombok.Builder;
+import lombok.Data;
+
+import org.springframework.data.annotation.Id;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+
+@Data
+@Builder(builderClassName = "Builder")
+@JsonDeserialize(builder = Book.Builder.class)
+public class Book {
+
+  @Id
+  private Integer id;
+
+  private String title;
+
+  @JsonPOJOBuilder(withPrefix = "")
+  public static class Builder {
+  }
+}
---
+++ data-pg-r2dbc/src/main/java/io/github/namdo/webfluxexamples/datapgr2dbc/service/BookService.java
@@ -0,0 +1,20 @@
+package io.github.namdo.webfluxexamples.datapgr2dbc.service;
+
+import io.github.namdo.webfluxexamples.datapgr2dbc.domain.Book;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import org.springframework.data.domain.Pageable;
+
+public interface BookService {
+
+  Mono<Book> save(Book book);
+
+  Flux<Book> findAllByPagination(Pageable pageable);
+
+  Flux<Book> findAll();
+
+  Mono<Book> findOne(Integer id);
+
+  Mono<Void> delete(Integer id);
+}
