1b8ece2b161ce783ae7ae878e92cd79513f35d87
namdo <namdodeveloper@gmail.com>
2021-12-10 14:43:40 -0700

    Initial commit

---
+++ data-postgres-liquibase-testcontainers/src/test/java/com/example/datapostgresliquibasetestcontainers/DataPostgresLiquibaseTestcontainersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datapostgresliquibasetestcontainers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataPostgresLiquibaseTestcontainersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
