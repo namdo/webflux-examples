f63bde7a94170430ae929fb4a168f64dfe29630d
Nam Do <nam.do@vivint.com>
2024-09-16 10:40:11 -0600

    Upgrade to Spring Boot 3.3.3

--- data-postgres-liquibase-testcontainers/src/main/java/io/github/namdo/webfluxexamples/datapostgresliquibasetestcontainers/web/BookController.java
+++ data-postgres-liquibase-testcontainers/src/main/java/io/github/namdo/webfluxexamples/datapostgresliquibasetestcontainers/web/BookController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
-@RequestMapping(BOOKS_PATH)
 @Log4j2
 @RequiredArgsConstructor
 public class BookController {
